fd277299b2976f020439aab6c024eb1833adda3a
bantherewind <bantherewind@gmail.com>
2014-11-10 10:13:07 -0800

    Fixed Exception constructor

--- Sampling.h
+++ Sampling.h
@@ -1,26 +1,67 @@
+/*
+ *
+ * Copyright (c) 2014, Ban the Rewind
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * Neither the name of the Ban the Rewind nor the names of its
+ * contributors may be used to endorse or promote products
+ * derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/*
+ * Sampling is a templated data processing library. This is a flexible way to
+ * sample and process an arbitrary data set with minimal code. The SampleT<T> 
+ * stores a vector of type T. It also keeps a map of processes which return type Y. 
+ * The second part of the map is a std::function which returns Y. The first part of 
+ * the map is a size_t, which represents your ID for the function. This is handy 
+ * for creating enumerators to organize your processes.
+ */
+
 #pragma once
 
 #include <vector>
 #include <map>
 
-/*
- * Templated sample processing. This is a flexible way to sample and process an 
- * arbitrary data set with minimal code. The SampleT<T> stores a vector of type T.
- * It also keeps a map of processes which return type Y. The second part of the map 
- * is a std::function which returns Y. The first part of the map is a size_t, 
- * which represents your ID for the function. This is handy for creating enumerators
- * to organize your processes.
- */
-
 namespace sampling {
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
-class Exception : public std::exception {
+class Exception : public std::exception
 {
 public:
-	Exception();
-	virtual ~Exception() throw() {}
+	Exception()
+	{
+	}
+	
+	virtual ~Exception() throw()
+	{
+	}
 	
 	inline const char*	what() const throw()
 	{ 
